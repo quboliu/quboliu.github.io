104c2f89bfd93f246852f91080e37c5e8822f56a
orb-agents <orb-agents@mail.com>
2026-09-24 11:14:42 -0400

    style: refine article presentation

--- src/utils/transformers/fileName.js
+++ src/utils/transformers/fileName.js
@@ -54,8 +54,8 @@ export const transformerFileName = ({
             ? "px-2"
             : "pl-4 pr-2 before:inline-block before:size-1 before:bg-green-500 before:rounded-full before:absolute before:top-[45%] before:left-2",
           style === "v1"
-            ? "left-0 -top-6 rounded-t-md border border-b-0 bg-muted/50"
-            : "left-2 top-(--file-name-offset) border rounded-md bg-background",
+            ? "left-0 -top-6 rounded-t border border-b-0 bg-muted/50"
+            : "left-2 top-(--file-name-offset) border rounded bg-background",
         ],
       },
       children: [
